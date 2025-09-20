9216a4992ba198f76f0a7902ec87d932f57854f6
dolfromspace <41292379+limeandcode@users.noreply.github.com>
2025-09-20 12:05:44 +1200

    refactor: pull aggregator out of main (#12)

--- supabase/functions/fetch-github-stats/types/pull-request-stat.ts
+++
@@ -1,8 +0,0 @@
-export type PullRequestStat = {
-  mergeTime: number;
-  reviewTime: number;
-  numberOfFileChanges: number;
-  numberOfCommits: number;
-  changedLinesOfCodeCount: number;
-  deployTime: number;
-};
