a73751dc13492872d310290b891be5d62d4fc1d6
dolfromspace <41292379+limeandcode@users.noreply.github.com>
2025-09-22 10:13:35 +1200

    feat: redirect to dashboard for logged in (#15)
    * feat: redirect to dashboard for logged in
    
    * Delete utils/convert-parameter.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 import { AuthButton } from "@/components/auth-button";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = await createClient();
+  const user = await supabase.auth.getUser();
+
+  if (user.data.user) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
